b4d64fe704f401f4cdbfe96257774b7e05a9132b
ANIKESH YADAV <anikydv38@gmail.com>
2025-02-18 14:49:10 +0530

    updated dirname import

--- server.js
+++ server.js
@@ -1,7 +1,54 @@
+// import express from "express";
+// import dotenv from "dotenv";
+// import path from "path";
+// // import connectDB from "./config/db";
+// import cors from "cors";
+// import {
+//   errorResponserHandler,
+//   invalidPathHandler,
+// } from "./middleware/errorHandler.js";
+
+// // Routes
+// import userRoutes from "./routes/userRoutes.js";
+// import postRoutes from "./routes/postRoutes.js";
+// import commentRoutes from "./routes/commentRoutes.js";
+// import postCategoriesRoutes from "./routes/postCategoriesRoutes.js";
+// import connectDB from "./config/db.js";
+
+// dotenv.config();
+// connectDB();
+// const app = express();
+// app.use(express.json());
+
+// const corsOptions = {
+//   exposedHeaders: "*",
+// };
+
+// app.use(cors(corsOptions));
+
+// app.get("/", (req, res) => {
+//   res.send("Server is running...");
+// });
+
+// app.use("/api/users", userRoutes);
+// app.use("/api/posts", postRoutes);
+// app.use("/api/comments", commentRoutes);
+// app.use("/api/post-categories", postCategoriesRoutes);
+
+// // static assets
+// app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+// app.use(invalidPathHandler);
+// app.use(errorResponserHandler);
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
-// import connectDB from "./config/db";
+import { fileURLToPath } from "url";  // Import to fix the __dirname issue
 import cors from "cors";
 import {
   errorResponserHandler,
@@ -34,6 +81,10 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
 app.use("/api/post-categories", postCategoriesRoutes);
+
+// Fix for __dirname in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // static assets
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
--- utils/fileRemover.js
+++ utils/fileRemover.js
@@ -1,5 +1,30 @@
+// import fs from "fs";
+// import path from "path";
+
+// const fileRemover = (filename) => {
+//   fs.unlink(path.join(__dirname, "../uploads", filename), function (err) {
+//     if (err && err.code == "ENOENT") {
+//       // file doesn't exist
+//       console.log(`File ${filename} doesn't exist, won't remove it.`);
+//     } else if (err) {
+//       console.log(err.message);
+//       console.log(`Error occured while trying to remove file ${filename}`);
+//     } else {
+//       console.log(`removed ${filename}`);
+//     }
+//   });
+// };
+
+// export { fileRemover };
+
+
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";  // Import to fix __dirname issue
+
+// Fix for __dirname in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const fileRemover = (filename) => {
   fs.unlink(path.join(__dirname, "../uploads", filename), function (err) {
@@ -8,7 +33,7 @@ const fileRemover = (filename) => {
       console.log(`File ${filename} doesn't exist, won't remove it.`);
     } else if (err) {
       console.log(err.message);
-      console.log(`Error occured while trying to remove file ${filename}`);
+      console.log(`Error occurred while trying to remove file ${filename}`);
     } else {
       console.log(`removed ${filename}`);
     }
