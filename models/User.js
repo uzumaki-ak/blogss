d44070a7449ac764395a785678926fd1bd0b1c5e
ANIKESH YADAV <anikydv38@gmail.com>
2025-02-18 14:34:23 +0530

    Fixed adminGuard typo and improved error handling

--- models/User.js
+++ models/User.js
@@ -1,5 +1,46 @@
+// import { Schema, model } from "mongoose";
+// import { hash, compare } from "bcryptjs";
+// import { sign } from "jsonwebtoken";
+
+// const UserSchema = new Schema(
+//   {
+//     avatar: { type: String, default: "" },
+//     name: { type: String, required: true },
+//     email: { type: String, required: true, unique: true },
+//     password: { type: String, required: true },
+//     verified: { type: Boolean, default: false },
+//     verificationCode: { type: String, required: false },
+//     admin: { type: Boolean, default: false },
+//   },
+//   { timestamps: true }
+// );
+
+// UserSchema.pre("save", async function (next) {
+//   if (this.isModified("password")) {
+//     this.password = await hash(this.password, 10);
+//     return next();
+//   }
+//   return next();
+// });
+
+// UserSchema.methods.generateJWT = async function () {
+//   return await sign({ id: this._id }, process.env.JWT_SECRET, {
+//     expiresIn: "30d",
+//   });
+// };
+
+// UserSchema.methods.comparePassword = async function (enteredPassword) {
+//   return await compare(enteredPassword, this.password);
+// };
+
+// const User = model("User", UserSchema);
+// export default User;
+
+
 import { Schema, model } from "mongoose";
-import { hash, compare } from "bcryptjs";
+import bcrypt from "bcryptjs";  // Import bcrypt as a default import
+const { hash, compare } = bcrypt;  // Destructure hash and compare
+
 import { sign } from "jsonwebtoken";
 
 const UserSchema = new Schema(
