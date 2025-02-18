70ed2bd110134fe6362ba26db95cd4e5f8c5e34d
ANIKESH YADAV <anikydv38@gmail.com>
2025-02-18 14:49:10 +0530

    updated dirname import

--- middleware/uploadPictureMiddleware.js
+++ middleware/uploadPictureMiddleware.js
@@ -1,5 +1,39 @@
+// import multer from "multer";
+// import path from "path";
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, path.join(__dirname, "../uploads"));
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, `${Date.now()}-${file.originalname}`);
+//   },
+// });
+
+// const uploadPicture = multer({
+//   storage: storage,
+//   limits: {
+//     fileSize: 1 * 1000000, // 1MB
+//   },
+//   fileFilter: function (req, file, cb) {
+//     let ext = path.extname(file.originalname);
+//     if (ext !== ".png" && ext !== ".jpg" && ext !== ".jpeg") {
+//       return cb(new Error("Only images are allowed"));
+//     }
+//     cb(null, true);
+//   },
+// });
+
+// export { uploadPicture };
+
+
 import multer from "multer";
 import path from "path";
+import { fileURLToPath } from "url";  // Import to fix __dirname issue
+
+// Fix for __dirname in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
